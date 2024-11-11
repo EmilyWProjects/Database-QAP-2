ed0d012a7ac0185957abd8beecf04a9e9c760287
EmilyWProjects <Emily.Warford@keyin.com>
2024-11-11 02:55:07 -0330

    Added data here as well

--- Problem2.sql
+++ Problem2.sql
@@ -35,3 +35,40 @@ CREATE TABLE order_items (
     FOREIGN KEY (product_id) REFERENCES products(id)
 
 );
+
+
+INSERT INTO products (product_name, price, stock_quantity) VALUES
+('Knife Set', 50.99, 20),
+('Mirror', 30.99, 20),
+('Doll', 20.99, 30),
+('Necklace', 80.99, 15),
+('Television', 200.99, 10);
+
+
+INSERT INTO customers (first_name, last_name, email) VALUES
+('Frederick', 'Kroger', 'frederick_kroger@example.com'),
+('Annie', 'Wilkerson', 'annie_wilkerson@example.com'),
+('Charles', 'Leroy', 'charles_leroy@example.com'),
+('Sarah', 'Morgan', 'sarah_morgan@example.com');
+('Carietta', 'Black', 'carietta_black@example.com');
+
+
+INSERT INTO orders (customer_id, order_date) VALUES
+(1, '2021-01-01'),
+(2, '2022-02-02'),
+(3, '2023-03-03'),
+(4, '2024-04-04'),
+(1, '2025-05-05');
+
+
+INSERT INTO order_items (order_id, product_id, quantity) VALUES
+(1, 1, 1),
+(1, 3, 5),
+(2, 2, 2),
+(2, 1, 1),
+(3, 3, 3),
+(3, 4, 4),
+(4, 4, 4),
+(4, 5, 3),
+(5, 5, 5),
+(5, 2, 2);
